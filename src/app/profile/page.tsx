ee0fd07a41d2c523041ebc5c3970c88cef620e01
Kay Roye <05kalusss@gmail.com>
2024-10-16 17:27:06 -0400

    Fixed querying

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -16,8 +16,8 @@ import { usePathname } from 'next/navigation';
 
 // Define what data we want to fetch from the server
 const GET_USER_PROFILE = gql`
-  query GetUserProfile($userId: ID!) {
-    user(id: $userId) {
+  query getUserById($userId: String!) {
+    getUserById(_id: $userId) {
       _id
       preferred_username
       profilePicture
@@ -66,6 +66,7 @@ export default function Profile() {
     variables: { userId: user?._id },
     skip: !user || loading, // Skip the query if user is not loaded yet
   });
+
   console.log(profileData);
 
   useEffect(() => {
@@ -147,18 +148,18 @@ export default function Profile() {
             <div className="w-full max-w-2xl bg-white shadow-md rounded-lg p-6">
               <div className="flex flex-col sm:flex-row items-center sm:items-start space-y-4 sm:space-y-0 sm:space-x-6">
                 <Image
-                  src={profileData?.user?.profilePicture || "/default_avatar.png"}
+                  src={profileData?.profilePicture || "/default_avatar.png"}
                   alt="Profile Picture"
                   width={100}
                   height={100}
                   className="rounded-full"
                 />
                 <div className="flex flex-col items-center sm:items-start">
-                  <h2 className="text-2xl font-bold">{profileData?.user?.preferred_username}</h2>
+                  <h2 className="text-2xl font-bold">{profileData?.getUserById?.preferred_username}</h2>
                   <div className="flex flex-wrap justify-center sm:justify-start gap-4 mt-2">
-                    <span className="text-gray-600">Polls: {profileData?.user?.posts?.length || 0}</span>
-                    <span className="text-gray-600">Followers: {profileData?.user?.followers?.length || 0}</span>
-                    <span className="text-gray-600">Following: {profileData?.user?.following?.length || 0}</span>
+                    <span className="text-gray-600">Polls: {profileData?.getUserById?.posts?.length || 0}</span>
+                    <span className="text-gray-600">Followers: {profileData?.getUserById?.followers?.length || 0}</span>
+                    <span className="text-gray-600">Following: {profileData?.getUserById?.following?.length || 0}</span>
                   </div>
                 </div>
               </div>
